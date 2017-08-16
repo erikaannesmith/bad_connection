ecaf0da3c26e334d29f36d57344a9644ee282d9e
Erika Smith <erikaannesmith@gmail.com>
2017-08-16 13:53:41 -0600

    final submission

---
+++ bad_connection.rb
@@ -0,0 +1,38 @@
+
+ready_to_quit = false
+puts "HELLO THIS IS A GROCERY STORE!"
+
+byecount = 0
+
+until ready_to_quit
+
+user_input = gets.chomp.to_s
+
+
+when "GOODBYE!"
+while user_input = gets.chomp.to_s
+  case GOODBYE
+  byecount +=1
+  puts byecount
+  break if byecount == 2
+else
+  byecount = 0
+  speak
+end
+
+  if
+    user_input == ""
+    puts "HELLO?!"
+  elsif
+    user_input != user_input.upcase
+    puts "I AM HAVING A HARD TIME HEARING YOU!"
+  elsif
+    user_input == "GOODBYE!"
+    puts "ANYTHING ELSE I CAN HELP WITH?"
+  elsif
+    user_input == user_input.upcase
+    puts "NO, THIS IS NOT A PETSTORE."
+  end
+end
+
+puts "THANKS FOR CALLING!"
